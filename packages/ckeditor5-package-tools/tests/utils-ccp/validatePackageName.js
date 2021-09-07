8e94c74c48de43dce536ecd0506aa14640304d73
Przemyslaw Zan <p.zan@cksource.com>
2021-09-07 13:20:25 +0200

    Added tests for validatePackageName function.

--- packages/ckeditor5-package-tools/tests/utils-ccp/validatePackageName.js
+++ packages/ckeditor5-package-tools/tests/utils-ccp/validatePackageName.js
@@ -5,30 +5,88 @@
 
 'use strict';
 
+const sinon = require( 'sinon' );
 const expect = require( 'chai' ).expect;
+const mockery = require( 'mockery' );
 
-describe( 'lib/utils', () => {
-	const validatePackageName = require( '../../../create-ckeditor5-plugin/lib/utils/validatePackageName' );
+describe( 'lib/utils-ccp/validatePackageName()', () => {
+	let validatePackageName, validateNpmPackageName;
 
-	describe( 'validatePackageName()', () => {
-		it( 'should be a function', () => {
-			expect( validatePackageName ).to.be.an( 'function' );
+	beforeEach( () => {
+		mockery.enable( {
+			useCleanCache: true,
+			warnOnReplace: false,
+			warnOnUnregistered: false
 		} );
 
-		it( 'work correctly with valid package name', () => {
-		// TODO
-		} );
+		validateNpmPackageName = sinon.stub();
 
-		it( 'stop the process if the package name is not valid name for a npm package', () => {
-		// TODO
-		} );
+		mockery.registerMock( 'validate-npm-package-name', validateNpmPackageName );
 
-		it( 'stop the process if the package name does not contain the "ckeditor5-" prefix', () => {
-		// TODO
-		} );
+		sinon.stub( console, 'info' );
 
-		it( 'stop the process if the package name contains only the "ckeditor5-" prefix', () => {
-		// TODO
-		} );
+		validatePackageName = require( '../../../create-ckeditor5-plugin/lib/utils/validatePackageName' );
+	} );
+
+	afterEach( () => {
+		sinon.restore();
+		mockery.disable();
+	} );
+
+	it( 'should be a function', () => {
+		expect( validatePackageName ).to.be.an( 'function' );
+	} );
+
+	it( 'returns true for a valid package name', () => {
+		validateNpmPackageName.returns( { validForNewPackages: true } );
+
+		const returnedValue = validatePackageName( 'ckeditor5-test-package' );
+
+		expect( returnedValue ).to.equal( true );
+	} );
+
+	it( 'returns false for a package name that is not valid name for a npm package', () => {
+		validateNpmPackageName.returns( { validForNewPackages: false } );
+		const consoleStub = sinon.stub( console, 'log' );
+
+		const returnedValue = validatePackageName( 'ckeditor5-test-package' );
+
+		consoleStub.restore();
+
+		expect( returnedValue ).to.equal( false );
+		expect( consoleStub.callCount ).to.equal( 1 );
+		expect( consoleStub.firstCall.args[ 0 ] ).to.equal(
+			'Provided <packageName> is not valid name for a npm package.'
+		);
+	} );
+
+	it( 'returns false for a package name that does not contain the "ckeditor5-" prefix', () => {
+		validateNpmPackageName.returns( { validForNewPackages: true } );
+		const consoleStub = sinon.stub( console, 'log' );
+
+		const returnedValue = validatePackageName( 'test-package' );
+
+		consoleStub.restore();
+
+		expect( returnedValue ).to.equal( false );
+		expect( consoleStub.callCount ).to.equal( 1 );
+		expect( consoleStub.firstCall.args[ 0 ] ).to.equal(
+			'Package name should follow the "ckeditor5-" prefix.'
+		);
+	} );
+
+	it( 'returns false for a package name that contains only the "ckeditor5-" prefix', () => {
+		validateNpmPackageName.returns( { validForNewPackages: true } );
+		const consoleStub = sinon.stub( console, 'log' );
+
+		const returnedValue = validatePackageName( 'ckeditor5-' );
+
+		consoleStub.restore();
+
+		expect( returnedValue ).to.equal( false );
+		expect( consoleStub.callCount ).to.equal( 1 );
+		expect( consoleStub.firstCall.args[ 0 ] ).to.equal(
+			'Package name should contain its name after the "ckeditor5-" prefix.'
+		);
 	} );
 } );
