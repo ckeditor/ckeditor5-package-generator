505d1f8e7fd73599a69a325a73b6504378a9a1b0
Kamil Piechaczek <pomek@users.noreply.github.com>
2021-09-09 08:13:09 +0200

    Tests should not depend on external variables.

--- packages/create-ckeditor5-plugin/tests/utils/get-latest-version-of-package.js
+++ packages/create-ckeditor5-plugin/tests/utils/get-latest-version-of-package.js
@@ -7,15 +7,29 @@
 
 const sinon = require( 'sinon' );
 const expect = require( 'chai' ).expect;
+const mockery = require( 'mockery' );
 
 describe( 'lib/utils/get-latest-version-of-package', () => {
-	let getLatestVersionOfPackage;
+	let getLatestVersionOfPackage, execSyncStub;
 
 	beforeEach( () => {
+		mockery.enable( {
+			useCleanCache: true,
+			warnOnReplace: false,
+			warnOnUnregistered: false
+		} );
+
+		execSyncStub = sinon.stub().returns( Buffer.from( '30.0.0' ) );
+
+		mockery.registerMock( 'child_process', {
+			execSync: execSyncStub
+		} );
+
 		getLatestVersionOfPackage = require( '../../lib/utils/get-latest-version-of-package' );
 	} );
 
 	afterEach( () => {
+		mockery.disable();
 		sinon.restore();
 	} );
 
@@ -23,14 +37,20 @@ describe( 'lib/utils/get-latest-version-of-package', () => {
 		expect( getLatestVersionOfPackage ).to.be.an( 'function' );
 	} );
 
-	it( 'returns a string', async () => {
-		const returnedValue = await getLatestVersionOfPackage( 'ckeditor5' );
+	it( 'returns a string', () => {
+		const returnedValue = getLatestVersionOfPackage( 'ckeditor5' );
 
 		expect( returnedValue ).to.be.a( 'string' );
 	} );
 
-	it( 'returns a valid SemVer number', async () => {
-		const returnedValue = await getLatestVersionOfPackage( 'ckeditor5' );
+	it( 'calls "npm show" to deterine the version', () => {
+		getLatestVersionOfPackage( 'ckeditor5' );
+
+		expect( execSyncStub.firstCall.firstArg ).to.equal( 'npm view ckeditor5 version' );
+	} );
+
+	it( 'returns a version matching semantic versioning specification', () => {
+		const returnedValue = getLatestVersionOfPackage( 'ckeditor5' );
 
 		expect( returnedValue ).to.match( /^\d+\.\d+\.\d+$/ );
 	} );
