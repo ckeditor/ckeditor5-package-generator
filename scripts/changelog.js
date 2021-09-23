ac0a20dc0bd8dd879b3d7b2fadf3afdc7e414a03
Kamil Piechaczek <pomek@users.noreply.github.com>
2021-09-23 09:28:36 +0200

    Introduced release scripts.

---
+++ scripts/changelog.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+/**
+ * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+'use strict';
+
+require( '@ckeditor/ckeditor5-dev-env' )
+	.generateChangelogForMonoRepository( {
+		cwd: process.cwd(),
+		packages: 'packages',
+		transformScope: name => {
+			if ( name === 'create-ckeditor5-plugin' ) {
+				return 'https://www.npmjs.com/package/create-ckeditor5-plugin';
+			}
+
+			return 'https://www.npmjs.com/package/@ckeditor/ckeditor5-' + name;
+		}
+	} );
