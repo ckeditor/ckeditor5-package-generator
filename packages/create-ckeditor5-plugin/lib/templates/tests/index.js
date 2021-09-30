1617477f1e389855305bf312aeaed98130e333d4
Kamil Piechaczek <pomek@users.noreply.github.com>
2021-09-30 14:37:30 +0200

    DLL: Added an icon to exported things.

--- packages/create-ckeditor5-plugin/lib/templates/tests/index.js
+++ packages/create-ckeditor5-plugin/lib/templates/tests/index.js
@@ -1,8 +1,16 @@
-import { MyPlugin as MyPluginDll } from '../src';
+import { MyPlugin as MyPluginDll, icons } from '../src';
 import MyPlugin from '../src/myplugin';
+
+import ckeditor from './../theme/icons/ckeditor.svg';
 
 describe( 'CKEditor5 MyPlugin DLL', () => {
 	it( 'exports MyPlugin', () => {
 		expect( MyPluginDll ).to.equal( MyPlugin );
+	} );
+
+	describe( 'icons', () => {
+		it( 'exports the "ckeditor" icon', () => {
+			expect( icons.ckeditor ).to.equal( ckeditor );
+		} );
 	} );
 } );
