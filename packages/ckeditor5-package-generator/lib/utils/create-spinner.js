ae4024ba02e7883be2afbcaf53d39754b8279a9d
Kamil Piechaczek <pomek@users.noreply.github.com>
2021-10-20 15:26:05 +0200

    Added a spinner when installing packages.

---
+++ packages/ckeditor5-package-generator/lib/utils/create-spinner.js
@@ -0,0 +1,73 @@
+/**
+ * @license Copyright (c) 2020-2021, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+'use strict';
+
+const isInteractive = require( 'is-interactive' );
+const cliSpinners = require( 'cli-spinners' );
+
+/**
+ * @param {String} title Description of the current processed task.
+ * @param {Object} options
+ * @param {Boolean} [options.isDisabled] Whether the spinner should be disabled.
+ * @return {CKEditor5Spinner}
+ */
+module.exports = function createSpinner( title, options ) {
+	const isEnabled = !options.isDisabled && isInteractive();
+
+	let timerId;
+
+	return {
+		start() {
+			if ( !isEnabled ) {
+				console.log( `📍 ${ title }` );
+				return;
+			}
+
+			const interval = options.interval || cliSpinners.dots12.interval;
+			const frames = cliSpinners.dots12.frames;
+
+			let index = 0;
+			let shouldClearLastLine = false;
+
+			timerId = setInterval( () => {
+				if ( index === frames.length ) {
+					index = 0;
+				}
+
+				if ( shouldClearLastLine ) {
+					clearLastLine();
+				}
+
+				process.stdout.write( `${ frames[ index++ ] } ${ title }` );
+				shouldClearLastLine = true;
+			}, interval );
+		},
+
+		finish() {
+			if ( !isEnabled ) {
+				return;
+			}
+
+			clearInterval( timerId );
+			clearLastLine();
+
+			console.log( `📍 ${ title }` );
+		}
+	};
+
+	function clearLastLine() {
+		process.stdout.clearLine();
+		process.stdout.cursorTo( 0 );
+	}
+};
+
+/**
+ * @typedef {Object} CKEditor5Spinner
+ *
+ * @property {Function} start
+ *
+ * @property {Function} finish
+ */
