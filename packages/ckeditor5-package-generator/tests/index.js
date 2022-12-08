e0f7b566873e0b53810fc068ccc4447e18f7ab2d
Przemyslaw Zan <p.zan@cksource.com>
2022-12-08 13:25:46 +0100

    Added failing test.

--- packages/ckeditor5-package-generator/tests/index.js
+++ packages/ckeditor5-package-generator/tests/index.js
@@ -111,6 +111,10 @@ describe( 'lib/index', () => {
 		sinon.restore();
 	} );
 
+	it( 'this test always fails', () => {
+		expect( 0 ).to.equal( 1 );
+	} );
+
 	it( 'should be a function', () => {
 		expect( index ).to.be.a( 'function' );
 	} );
