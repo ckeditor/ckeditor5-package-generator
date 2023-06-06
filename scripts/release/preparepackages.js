1600fddef0f4e0f8b2b1a3b2cfb033be3417b1c4
Marcin Panek <m.panek@cksource.com>
2023-06-06 16:29:36 +0200

    Internal: Aligned release tools to the latest CKEditor 5 dev.

---
+++ scripts/release/preparepackages.js
@@ -0,0 +1,91 @@
+#!/usr/bin/env node
+
+/**
+ * @license Copyright (c) 2020-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/* eslint-env node */
+
+'use strict';
+
+const { Listr } = require( 'listr2' );
+const releaseTools = require( '@ckeditor/ckeditor5-dev-release-tools' );
+const parseArguments = require( './utils/parsearguments' );
+const { PACKAGES_DIRECTORY, RELEASE_DIRECTORY } = require( './utils/constants' );
+
+const cliArguments = parseArguments( process.argv.slice( 2 ) );
+const latestVersion = releaseTools.getLastFromChangelog();
+const versionChangelog = releaseTools.getChangesForVersion( latestVersion );
+
+const tasks = new Listr( [
+	{
+		title: 'Verifying the repository.',
+		task: async () => {
+			const errors = await releaseTools.validateRepositoryToRelease( {
+				version: latestVersion,
+				changes: versionChangelog,
+				branch: 'master'
+			} );
+
+			if ( !errors.length ) {
+				return;
+			}
+
+			return Promise.reject( 'Aborted due to errors.\n' + errors.map( message => `* ${ message }` ).join( '\n' ) );
+		}
+	},
+	{
+		title: 'Updating the `#version` field.',
+		task: () => {
+			return releaseTools.updateVersions( {
+				packagesDirectory: PACKAGES_DIRECTORY,
+				version: latestVersion
+			} );
+		}
+	},
+	{
+		title: 'Copying `package-generator` packages to the release directory.',
+		task: () => {
+			return releaseTools.prepareRepository( {
+				outputDirectory: RELEASE_DIRECTORY,
+				packagesDirectory: PACKAGES_DIRECTORY,
+				packagesToCopy: cliArguments.packages
+			} );
+		}
+	},
+	{
+		title: 'Cleaning-up.',
+		task: () => {
+			return releaseTools.cleanUpPackages( {
+				packagesDirectory: RELEASE_DIRECTORY,
+				packageJsonFieldsToRemove: [
+					// TODO: Preserve `scripts.postinstall`.
+					// See: https://github.com/ckeditor/ckeditor5/issues/14318.
+					'devDependencies',
+					'depcheckIgnore'
+				]
+			} );
+		}
+	},
+	{
+		title: 'Commit & tag.',
+		task: () => {
+			return releaseTools.commitAndTag( {
+				version: latestVersion,
+				files: [
+					'package.json',
+					`${ PACKAGES_DIRECTORY }/*/package.json`
+				]
+			} );
+		}
+	}
+] );
+
+tasks.run()
+	.catch( err => {
+		process.exitCode = 1;
+
+		console.log( '' );
+		console.error( err );
+	} );
---
+++ scripts/release/utils/constants.js
@@ -0,0 +1,9 @@
+/**
+ * @license Copyright (c) 2020-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+module.exports = {
+	PACKAGES_DIRECTORY: 'packages',
+	RELEASE_DIRECTORY: 'release'
+};
