93f58fdb02b23ec97cc1778c0731db4cbfc8789a
Piotr Szczęśniak <szczesniakp@o2.pl>
2024-06-18 10:06:27 +0200

    Update eslint config and add new rule for NIM templates.

--- packages/ckeditor5-package-generator/lib/templates/ts/.eslintrc.cjs
+++ packages/ckeditor5-package-generator/lib/templates/ts/.eslintrc.cjs
@@ -20,6 +20,10 @@ module.exports = {
 	rules: {
 		//  This rule disallows importing from any path other than the package main entrypoint.
 		'ckeditor5-rules/allow-imports-only-from-main-package-entry-point': 'error',
+		// This rule ensures that all imports from `@ckeditor/*` packages are done through the main package entry points.
+		// This is required for the editor types to work properly and to ease migration to the installation methods
+		// introduced in CKEditor 5 version 42.0.0.
+		'ckeditor5-rules/no-legacy-imports': 'error',
 		// As required by the ECMAScript (ESM) standard, all imports must include a file extension.
 		// If the import does not include it, this rule will try to automatically detect the correct file extension.
 		'ckeditor5-rules/require-file-extensions-in-imports': [
