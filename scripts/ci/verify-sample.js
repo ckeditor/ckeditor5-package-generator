b88fa43e01de228264d394cdbc585b09611e2bb3
Paweł Smyrek <p.smyrek@cksource.com>
2025-01-08 11:45:58 +0100

    Bumped the year.

--- scripts/ci/verify-sample.js
+++ scripts/ci/verify-sample.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 /**
- * @license Copyright (c) 2020-2024, CKSource Holding sp. z o.o. All rights reserved.
+ * @license Copyright (c) 2020-2025, CKSource Holding sp. z o.o. All rights reserved.
  * For licensing, see LICENSE.md.
  */
 
