4fb23168659a9b51ef8e90abafb45dab8fca4334
Filip Sobol <filipsobol@users.noreply.github.com>
2025-07-16 15:48:47 +0200

    Use `require.resolve` to get `@ckeditor/ckeditor5-theme-lark`.

--- packages/ckeditor5-package-tools/lib/utils/get-theme-path.js
+++ packages/ckeditor5-package-tools/lib/utils/get-theme-path.js
@@ -3,21 +3,16 @@
  * For licensing, see LICENSE.md.
  */
 
-import fs from 'fs-extra';
-import path from 'path';
+import module from 'module';
 
 /**
  * Returns an absolute path to the main file of the `@ckeditor/ckeditor5-theme-lark` package.
- *
- * The function does the same as what does `require.resolve()`. However, there is no option for mocking it in tests,
- * hence the value is obtained manually.
  *
  * @param {String} cwd
  * @return {String}
  */
 export default function getThemePath( cwd ) {
-	const packagePath = path.join( cwd, 'node_modules', '@ckeditor', 'ckeditor5-theme-lark' );
-	const packageJson = fs.readJsonSync( path.join( packagePath, 'package.json' ) );
-
-	return path.join( packagePath, packageJson.main );
+	return module
+		.createRequire( import.meta.url )
+		.resolve( '@ckeditor/ckeditor5-theme-lark', { paths: [ cwd ] } );
 }
--- packages/ckeditor5-package-tools/tests/utils/get-theme-path.js
+++ packages/ckeditor5-package-tools/tests/utils/get-theme-path.js
@@ -3,34 +3,32 @@
  * For licensing, see LICENSE.md.
  */
 
-import { describe, it, expect, vi, beforeEach } from 'vitest';
-import fs from 'fs-extra';
+import { describe, it, expect, vi } from 'vitest';
+import module from 'module';
 import getThemePath from '../../lib/utils/get-theme-path.js';
 
-vi.mock( 'path', () => ( {
-	default: {
-		join: ( ...chunks ) => chunks.join( '/' ).replace( '/./', '/' )
-	}
-} ) );
-vi.mock( 'fs-extra' );
+const MOCK_PATH = '/process/cwd/node_modules/@ckeditor/ckeditor5-theme-lark/theme/theme.css';
 
 describe( 'lib/utils/get-theme-path', () => {
-	beforeEach( () => {
-		vi.mocked( fs.readJsonSync ).mockImplementation( filePath => {
-			if ( filePath === '/process/cwd/node_modules/@ckeditor/ckeditor5-theme-lark/package.json' ) {
-				return {
-					name: '@ckeditor/ckeditor5-theme-lark',
-					main: './theme/theme.css'
-				};
-			}
-		} );
-	} );
-
 	it( 'should be a function', () => {
 		expect( getThemePath ).toBeTypeOf( 'function' );
 	} );
 
 	it( 'returns an absolute path to an entry file of the "@ckeditor/ckeditor5-theme-lark" package', () => {
-		expect( getThemePath( '/process/cwd' ) ).toEqual( '/process/cwd/node_modules/@ckeditor/ckeditor5-theme-lark/theme/theme.css' );
+		const resolveSpy = vi.fn().mockReturnValue( MOCK_PATH );
+
+		vi
+			.spyOn( module, 'createRequire' )
+			.mockImplementationOnce( () => ( {
+				resolve: resolveSpy
+			} ) );
+
+		const cwd = '/process/cwd';
+
+		expect( getThemePath( cwd ) ).toEqual( MOCK_PATH );
+		expect( resolveSpy ).toHaveBeenCalledExactlyOnceWith(
+			expect.anything(),
+			expect.objectContaining( { paths: [ cwd ] } )
+		);
 	} );
 } );
