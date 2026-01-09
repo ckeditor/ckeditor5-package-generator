10ad454f013d1489c9f80796828c4ce386184ec0
Marcin Panek <m.panek@cksource.com>
2026-01-09 11:13:13 +0100

    Moved the SIGINT error handling to the main part of the package generator script.

--- packages/ckeditor5-package-generator/lib/utils/validate-package-name.js
+++ packages/ckeditor5-package-generator/lib/utils/validate-package-name.js
@@ -31,27 +31,16 @@ export default async function validatePackageName( logger, packageName ) {
 	logger.info( 'The provided package name:   ' + chalk.red( packageName || '' ) );
 	logger.info( 'Allowed characters list:     ' + chalk.blue( '0-9 a-z - . _' ) );
 
-	const result = await inquirer.prompt( {
+	const { validPackageName } = await inquirer.prompt( {
 		required: true,
 		message: 'Enter the valid package name:',
 		type: 'input',
 		name: 'validPackageName',
 		validate: name => validator( name ),
 		default: packageName
-	} ).catch( error => {
-		// Ctrl+C or prompt cancellation.
-		if ( error.message && error.message.includes( 'SIGINT' ) ) {
-			process.exit( 1 );
-		} else {
-			throw error;
-		}
 	} );
 
-	if ( !result ) {
-		return null;
-	}
-
-	return result.validPackageName;
+	return validPackageName;
 }
 
 /**
