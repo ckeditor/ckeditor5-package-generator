3b1d201a077653b6af27099c39f5e7799cf77089
Filip Sobol <filipsobol@users.noreply.github.com>
2026-01-14 14:32:36 +0100

    Remove tests for deleted code.

--- packages/ckeditor5-package-tools/tests/index.js
+++ packages/ckeditor5-package-tools/tests/index.js
@@ -4,35 +4,14 @@
  */
 
 import { describe, it, expect, vi } from 'vitest';
-import start from '../lib/tasks/start.js';
 import synchronizeTranslations from '../lib/tasks/synchronize-translations.js';
 import tasks from '../lib/index.js';
 
-vi.mock( '../lib/tasks/start.js' );
 vi.mock( '../lib/tasks/synchronize-translations.js' );
 
 describe( 'lib/index', () => {
 	it( 'should be an object containing available tasks', () => {
 		expect( tasks ).toBeTypeOf( 'object' );
-	} );
-
-	describe( '#start', () => {
-		it( 'is available', () => {
-			expect( tasks.start ).toBeTypeOf( 'function' );
-		} );
-
-		it( 'executes the proper function from the "tasks/" directory', () => {
-			tasks.start();
-
-			expect( start ).toBeCalledTimes( 1 );
-		} );
-
-		it( 'passes arguments directly to the function', () => {
-			const options = { foo: 1, bar: true };
-			tasks.start( options );
-
-			expect( start ).toHaveBeenCalledWith( options );
-		} );
 	} );
 
 	describe( '#translations:synchronize', () => {
